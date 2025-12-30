a1ba3f3005c2b4a10f919196084416b7c3d4ff09
krish0075 <THAMATAMKRISHNAREDDY123@GMAIL.COM>
2025-12-31 00:47:55 +0530

    fibonacci_number

--- fibonacci_number.py
+++ fibonacci_number.py
@@ -1,7 +1,7 @@
 def fibonacci(n):
-    if n == 0 or n == 1:
+    if n == 0 or n == 1: # Stopping condition
         return n
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n-1) + fibonacci(n-2) # Flow
 
 if __name__ == '__main__':
     print(fibonacci(9))
