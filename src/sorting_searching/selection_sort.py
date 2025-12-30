14016c14bed8e2a86ff7d000366b704b235f66a1
krish0075 <THAMATAMKRISHNAREDDY123@GMAIL.COM>
2025-12-31 02:21:55 +0530

    selection sort

---
+++ src/sorting_searching/selection_sort.py
@@ -0,0 +1,24 @@
+def selection_sort(arr):
+    """
+    Sorts a list of numbers using a Selection Sort algorithm.
+
+    INTUITION:
+    Maintain two subsets: sorted and unsorted. In every iteration, find the minimum element from the unsorted subset and
+    'select' it to move to the end of sorted subset.
+
+    Time Complexity: O(N^2) - Two nested loops.
+    Space Complexity: O(1) - In-place sorting.
+    """
+    n = len(arr)
+    for i in range(n):
+        # Assume the current position is the minimum
+        min_index = i
+
+        # Scan the rest of the list to find the actual minimum
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
---
+++ tests/test_sorting.py
@@ -0,0 +1,13 @@
+import pytest
+from src.sorting_searching.selection_sort import selection_sort
+
+
+@pytest.mark.parametrize("input_arr, expected", [
+    ([5, 7, 8, 4,1], [1, 4, 5, 7, 8]), # Standard Unsorted
+    ([1, 2,3], [1, 2, 3]),             # Sorted
+    ([], []),                          # Empty list
+    ([1], [1]),                        # Single Element
+    ([5, 2, 5, 1], [1, 2, 5, 5])       # Duplicate Element
+])
+def test_selection_sort(input_arr, expected):
+    assert selection_sort(input_arr) == expected
